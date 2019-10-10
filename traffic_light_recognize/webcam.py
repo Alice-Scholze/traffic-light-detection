a46ee1a738e50c4164971c5137ccd2b70ea1ec0f
Alice Scholze <alicescholze@outlook.com>
2019-10-09 22:39:41 -0300

    Deteccao de cores do semaforo

--- traffic_light_recognize/webcam.py
+++ traffic_light_recognize/webcam.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 classificador = cv2.CascadeClassifier("cascade_semaforo_2.xml")
 
@@ -10,6 +11,14 @@ while True:
     deteccoes = classificador.detectMultiScale(imagemCinza, scaleFactor=1.25, minNeighbors=7)
     for(x, y, l, a) in deteccoes:
         cv2.rectangle(imagem, (x, y), (x + l, y + a), (0, 255, 0), 2)
+        roi_color = imagem[y:y + a, x:x + l]
+        hsv = cv2.cvtColor(roi_color, cv2.COLOR_BGR2HSV)
+        green = cv2.inRange(hsv, (36, 25, 25), (70, 255, 255))
+        red = cv2.inRange(hsv, (161, 155, 84), (179, 255, 255))
+        yellow = cv2.inRange(hsv, (20, 100, 100), (30, 255, 255))
+        print('green: ', np.count_nonzero(green))
+        print('yellow: ', np.count_nonzero(yellow))
+        print('red: ', np.count_nonzero(red))
     cv2.imshow("detect", imagem)
     cv2.waitKey(1)
 camera.release()
